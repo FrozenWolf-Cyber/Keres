700d4c832d5a80b8ac7c48541914ea883439f69c
FrozenWolf <57902078+FrozenWolf-Cyber@users.noreply.github.com>
2020-09-30 15:26:20 +0530

    Delete player2.py

--- player2.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-# print(requests.post('http://127.0.0.1:5000/', {"match_making":'cJVrpLH389mGv&8&3&10&0'}).text)
-print(requests.post('http://127.0.0.1:5000/', {"check_for_match":'cJVrpLH389mGv'}).text)
